38782b9809ddc4ccfefba97c0ee405c060c6df19
damirdine <damirdine@outlook.fr>
2022-06-21 11:41:06 +0200

    contact pages

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Tooltip } from 'react-bootstrap';
 import TopBar from './components/TopBar';
 import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <BrowserRouter>
       <TopBar/>
       <Routes>
-        <Route path="/ABout" element={<About/>}/>
+        <Route path="/About" element={<About/>}/>
+      </Routes>
+      <Routes>
+        <Route path="/Contact" element={<Contact/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Container,Row,Col} from 'react-bootstrap';
+
+const Contact = () => {
+    return(
+        <Container className='mt-5'>
+            <Row>
+                <Col>
+                    <h1>Pizza Delicious</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia expedita incidunt velit nemo aliquam! Ullam numquam ipsam impedit sit. Aliquam debitis commodi dolor nemo consequatur, quasi neque veritatis quos optio esse praesentium quae in inventore ipsum saepe ullam assumenda hic fugiat, numquam quas aspernatur. Maiores, omnis maxime dolore, vel ullam perspiciatis ipsam laudantium reiciendis blanditiis animi ea vitae quis cupiditate enim labore consectetur quos delectus temporibus veniam repudiandae nostrum tenetur saepe aliquid. Ex, labore tempore.z</p>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+export default Contact
