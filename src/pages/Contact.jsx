4d61e671948311eedfadaa3d8a70ced9df08b0e1
damirdine <damirdine@outlook.fr>
2022-06-21 13:46:57 +0200

    Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,48 @@
 import React from 'react';
-import {Container,Row,Col} from 'react-bootstrap';
+import {Container,Row,Col,Table,Image} from 'react-bootstrap';
+import {FiPhoneCall} from 'react-icons/fi';
+import {ImMobile} from 'react-icons/im';
+import {AiOutlineMail, AoOutlineMail} from 'react-icons/ai'
 
 const Contact = () => {
     return(
         <Container className='mt-5'>
             <Row>
-                <Col>
+                <Col md={6}>
                     <h1>Pizza Delicious</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia expedita incidunt velit nemo aliquam! Ullam numquam ipsam impedit sit. Aliquam debitis commodi dolor nemo consequatur, quasi neque veritatis quos optio esse praesentium quae in inventore ipsum saepe ullam assumenda hic fugiat, numquam quas aspernatur. Maiores, omnis maxime dolore, vel ullam perspiciatis ipsam laudantium reiciendis blanditiis animi ea vitae quis cupiditate enim labore consectetur quos delectus temporibus veniam repudiandae nostrum tenetur saepe aliquid. Ex, labore tempore.z</p>
+                    <h2>Notre adresse:</h2>
+                    <p>103 rue de la rue,</p>
+                    <p>75021 Paris</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia expedita incidunt velit nemo aliquam! Ullam numquam ipsam impedit sit. </p>
+                    <Table striped bordered hover>
+                    <thead className='text-center bg-warning'>
+                        <td className='font-weight-bold' colSpan={4}>-- Nos Coordonées --</td>
+                    </thead>
+                    <tbody>
+                        <tr className="text-center">
+                        <td><FiPhoneCall/></td>
+                        <td>Téléphone</td>
+                        <td colSpan={2}>01 09 83 20 20</td>
+                        </tr>
+                        <tr className="text-center">
+                        <td><ImMobile/></td>
+                        <td>Portable</td>
+                        <td colSpan={2}>01 09 83 20 20</td>
+                        </tr>
+                        <tr className="text-center">
+                        <td><AiOutlineMail/></td>
+                        <td>Mail</td>
+                        <td text="center">contact@twitter.fr</td>
+                        </tr>
+                    </tbody>
+                    </Table>
+                </Col>
+                <Col md={6}>
+                    <Image src='./images/farmhouse.jpg' style={{
+                        width:"100%",
+                        height:"100%",
+                        objectFit:"cover"
+                    }}/>
                 </Col>
             </Row>
         </Container>
