7b65ebe35e2c92738576f03c8cf31f11e4fb8526
damirdine <damirdine@outlook.fr>
2022-07-01 22:02:06 +0200

    restruct' '

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,26 @@
 const express = require('express');
-
+const cors = require('cors')
 // Database
 const mongo = require("mongodb");
 const monk = require("monk");
+
+const app = express();
+const port = 8080
+app.use(cors());
+
 const db = monk("localhost:27017/pizzas");
 var collection = db.get('users');
 collection.find({},{},function(e,docs){
   docs;
 });
 
-const pizzasRouter = require('./routes/pizzas');
-const usersRouter = require('./routes/users')
-const cors = require('cors')
-
-const app = express();
-const port = 8080
-app.use(cors());
-
 app.use(function(req,res,next){
   req.db = db;
   next();
-  });
+});
+
+const pizzasRouter = require('./routes/pizzas');
+const usersRouter = require('./routes/users')
 
 app.use('/pizzas', pizzasRouter);
 app.use('/users', usersRouter);
