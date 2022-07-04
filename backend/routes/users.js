865b39befb63974ffc31d7584eba2ca04a7f5e80
damirdine <damirdine@outlook.fr>
2022-07-04 12:24:47 +0200

    get input value

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,90 +6,47 @@ router.get('/', function(req, res, next) {
     var db = req.db;
     var collection = db.get('users');
     collection.find({},{},function(e,docs){
-      res.json(docs)
+      res.json({docs})
     });
 });
 
-// router.post('/adduser',function(req,res){
-//   var db = req.db;
-  
-//   //post value from the form 
+router.post('/adduser',function(req,res){
+  var db = req.db;
+  //post value from the form 
 
-//   var userName = req.body.username;
-//   var userEmail = req.body.useremail;
-//   var userFullName = req.body.userfullname;
-//   var userAge = req.body.userage;
-//   var userLocation = req.body.userlocation;
-//   var userGender = req.body.usergender;
-//   var actionType = req.body.actiontype;
-//   var updateId = req.body.updateid;
+  let userEmail = req.body.useremail;
+  let userFullName = req.body.userfullname;
+  let userPassword = req.body.userPassword
 
-//   //Set the collection
-//   var collection = db.get('users');
-//   switch (actionType) {
-//     case 'edit':
-//       collection.update({ '_id' : updateId },{$set :
-//         {
-//           "username" : userName,
-//           "email": userEmail,
-//           "fullname":userFullName,
-//           "age":userAge,
-//           "location":userLocation,
-//           "gender":userGender
-//         }},
-//         function(err){
-//           if(err){
-//             res.send("Problem for UPDATING user database.")
-//           }
-//           res.redirect("userlist");
-//         });
-//       break;
-//     case 'add':
-//     default:
-//       collection.insert(
-//         {
-//           "username" : userName,
-//           "email": userEmail,
-//           "fullname":userFullName,
-//           "age":userAge,
-//           "location":userLocation,
-//           "gender":userGender
-//         }, 
-//         function(err){
-//           if(err){
-//             res.send("Problem for adding user to database.")
-//           }
-//           res.redirect("userlist");
-//         }
-//       )
-    
-//       break;
-//   }
-  
-// });
 
-// router.get('/:id', function(req,res){
-//   var db =req.db;
-//   var userToFind = req.params.id;
-//   var collection = db.get('userlist');
-//   collection.findOne({"_id": userToFind},{},function(e,docs){
-//     res.json(docs);
-//   })
-// });
+  //Set the collection
+  var collection = db.get('users');
+      collection.insert(
+        {
+          "fullname":userFullName,
+          "email": userEmail,
+          "password":userPassword,
+          "location":userLocation,
+          "gender":userGender
+        }, 
+        function(err){
+          if(err){
+            res.send("Problem for adding user to database.")
+          }
+          res.redirect("userlist");
+        }
+      )
+});
 
-// router.get('/deleteuser/:id', function(req,res){
-//   var db=req.db;
-//   var collection =db.get('userlist');
-//   var userToDelete = req.params.id;
-//   collection.remove({'_id':userToDelete},function(err){
-//     if(err){
-//       res.send("Problem for delete in user in the database");
-//     }
-//     else{
-//       res.redirect("/users/userlist");
-//     }
-//   });
-// });
+router.get('/:id', function(req,res){
+  var db =req.db;
+  var userToFind = req.params.id;
+  var collection = db.get('userlist');
+  collection.findOne({"_id": userToFind},{},function(e,docs){
+    res.json(docs);
+  })
+});
+
 
 
 module.exports = router;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,4 @@
+import React,{useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {Container} from 'react-bootstrap';
@@ -5,36 +6,66 @@ import { Link } from "react-router-dom"
 import AdressFrom from '../components/AdressFrom';
 
 function Register() {
-  return (
-    <Container className='mt-4'>
-        <h3>Register</h3>
-        <Form className='mt-4'>
-            <Form.Group className="mb-3" controlId="formBasicName">
-                <Form.Label>Name & Firstname</Form.Label>
-                <Form.Control type="text" placeholder="Jhon Doe" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control className="mb-2" type="password" placeholder="Password" />
-                <Form.Label >Confirm password</Form.Label>
-                <Form.Control type="password" placeholder="Confirm password" />
-            </Form.Group>
-            <details className='mb-3'>
-                <summary className='mb-3'>Add my adress (for your next order)</summary>
-                <AdressFrom/>
-            </details>
-            <Button variant="primary" type="submit">Register</Button> 
-            <br/><span className='d-block mt-4'>Or</span>
-            <Link to="/Login">Login</Link>
-        </Form>
-    </Container>
+    function addUser(e){
+        e.preventDefault()
+        let fullname = document.querySelector("#formBasicFullName").value
+        let email = document.querySelector("#formBasicEmail").value
+        let password = document.querySelector("#formBasicPassword").value
+        let confirmPassword = document.querySelector("#formBasicConfirmPassword").value
+        let phoneNumber = document.querySelector("#formGroupPhoneNumber").value
+        let adress = document.querySelector("#formGroupAdress").value
+        let complement = document.querySelector("#formGroupComplement").value
+        let postCode = document.querySelector("#formGroupPostCode").value
+        let city = document.querySelector("#formGroupCity").value
+
+        let user = {
+            fullname,
+            email,
+            password,
+            phone_number : phoneNumber,
+            adress : {
+                adress,
+                complement,
+                postcode:postCode,
+                city,
+            }
+        }
+        
+
+        console.log(user)
+    }
+    return (
+        <Container className='mt-4'>
+            <h3>Register</h3>
+            <Form onSubmit={addUser} className='mt-4' id='registerForm'>
+                <Form.Group className="mb-3" controlId="formBasicFullName">
+                    <Form.Label>Full name</Form.Label>
+                    <Form.Control type="text" placeholder="Jhon Doe" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control className="mb-2" type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+                    <Form.Label >Confirm password</Form.Label>
+                    <Form.Control type="password" placeholder="Confirm password" />
+                </Form.Group>
+                <details className='mb-3'>
+                    <summary className='mb-3'>Add my adress (for your next order)</summary>
+                    <AdressFrom/>
+                </details>
+                <Button variant="primary" type='submit'>Register</Button> 
+                <br/><span className='d-block mt-4'>Or</span>
+                <Link to="/Login">Login</Link>
+            </Form>
+        </Container>
   );
 }
 
