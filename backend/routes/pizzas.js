0b37aa68d5abefb917899b29e8d30c4f22101b37
damirdine <damirdine@outlook.fr>
2022-07-06 12:05:27 +0200

    package for front

--- backend/routes/pizzas.js
+++ backend/routes/pizzas.js
@@ -7,18 +7,6 @@ router.get('/', function (req, res) {
     collection.find({}, {}, function (err, docs) {
         res.json(docs);
     });
-  });
-
-
-router.get("/:id", function(req, res){
-  const db = req.db;
-
-  const pizzaToFind = req.params.id;
-
-  const collection = db.get("pizzas");
-  collection.findOne({"_id": pizzaToFind},{},function(e,docs){
-    res.json(docs);
-  });
 });
 
 module.exports = router;
