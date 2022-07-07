73f10b24aa71391f973d442be87c317997977739
damirdine <damirdine@outlook.fr>
2022-07-07 10:35:20 +0200

    basse passport user

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,38 @@
-const express = require('express');
+const express = require('express')
 const cors = require('cors')
 const bp = require("body-parser")
 // Database
 const mongo = require("mongodb")
 const monk = require("monk")
-const db = monk("localhost:27017/pizzas");
+const db = monk("localhost:27017/pizzas")
+// user passport
+const passport = require("passport")
+const session = require("express-session");
+// hasher
+const bcrypt = require("bcrypt");
 
 const app = express()
 const port = 8080
 
-app.use(cors())
-app.use(bp.json())
-app.use(bp.urlencoded({extended:true}))
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
 
-var collection = db.get('users');
-collection.find({},{},function(e,docs){
-  docs;
-});
+app.use(bp.json())  
+app.use(bp.urlencoded({extended:true})) 
+app.use(cors())
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+)
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(function(req,res,next){
   req.db = db;
