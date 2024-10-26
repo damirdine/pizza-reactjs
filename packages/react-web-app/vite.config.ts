095e82c001cd6c0994d5e84b370c7718ff12ea90
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2024-10-26 16:42:03 +0200

    chore: config build output folder

--- packages/react-web-app/vite.config.ts
+++ packages/react-web-app/vite.config.ts
@@ -1,7 +1,11 @@
+import path from 'node:path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const __direname = import.meta.dirname
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {outDir: path.resolve(__direname, "..", "..", "frontend")}
 })
