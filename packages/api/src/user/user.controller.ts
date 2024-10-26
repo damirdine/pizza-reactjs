ed4afb2847ceeda8cfa45b4a65bfdb93cdf02833
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2024-10-26 12:16:07 +0200

    feat(backend): add user service

--- packages/api/src/user/user.controller.ts
+++ packages/api/src/user/user.controller.ts
@@ -1,11 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
   @Get()
+  index() {
+    return this.userService.index();
+  }
+
+  @Get('/me')
   me() {
     return this.userService.me();
+  }
+
+  @Post('/register')
+  register() {
+    return this.userService.register();
+  }
+
+  @Post('/login')
+  login() {
+    return this.userService.login();
   }
 }
--- packages/api/src/user/user.service.ts
+++ packages/api/src/user/user.service.ts
@@ -2,13 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
+  private users = [
+    {
+      email: 'hello@me.com',
+      roles: [''],
+      authorizations: [],
+    },
+  ];
   me() {
     return {
-      data: {
-        email: 'hello@me.com',
-        roles: [''],
-        authorizations: [],
-      },
+      data: this.users[0],
     };
+  }
+  index() {
+    return { data: this.users };
+  }
+  register() {
+    return this.me();
+  }
+  login() {
+    return this.me();
   }
 }
